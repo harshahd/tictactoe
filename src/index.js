cb616bd6c4a4379908a4aecc973929c8dcc206df
Harsha HD <hdharsha4@gmail.com>
2021-04-30 06:31:02 +0530

    Added keyboard operability for grid cells, displays a draw message if the game is draw.

--- src/index.js
+++ src/index.js
@@ -85,8 +85,24 @@ const desc = move ?        'Go to move #' + move :        'Go to game start';
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
-    } else {
+    } 
+else
+{
+let draw;
+for(let count=0;count<current.squares.length;count++)
+{
+draw=current.squares[count]!=null;
+if(!draw && count<=9)
+break;
+}
+if(draw)
+{
+      status = 'The game ended in a draw';
+}
+else
+{
       status = 'Next player to be played: ' + (this.state.xIsNext ? 'X' : 'O');
+}
     }
     return (
       <div className="game" role="region" aria-label="game">
@@ -131,4 +147,49 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
+const left=37;
+const up=38;
+const right=39;
+const down=40;
+var squares=document.getElementsByClassName("square");
+for(let countSquare=0;countSquare<squares.length;countSquare++)
+{
+squares[countSquare].addEventListener('keydown', function(e)
+{
+if(countSquare%3==0)
+{
+if(e.which==right)
+{
+squares[countSquare+1].focus();
+}
+}
+if(countSquare%3==1)
+{
+if(e.which==right)
+{
+squares[countSquare+1].focus();
+}
+else if(e.which==left)
+{
+squares[countSquare-1].focus();
+}
+}
+if(countSquare%3==2)
+{
+if(e.which==left)
+{
+squares[countSquare-1].focus();
+}
+}
+if(e.which==up)
+{
+if(countSquare-3>=0)
+squares[countSquare-3].focus();
+}
+else if(e.which==down)
+{
+if(countSquare+3<9)
+squares[countSquare+3].focus();
+}
+});
+}
